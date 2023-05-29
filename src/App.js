8aa8d3a52608e39c7d9f987af1dbc3378397642c
gonzalomsastre <gonzalo.sastre@creativecoeffienct.net>
2023-05-29 15:53:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./assets/Logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h3>Próximamente en stores</h3>
+    </div>
+  );
+}
+
+export default App;
